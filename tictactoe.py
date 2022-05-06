baf188af5ca0f15a3664c984ba7ec485317ac286
Sadish Ghimire <102663365+sadishghimire1@users.noreply.github.com>
2022-05-06 18:56:44 +0545

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,30 @@
+from tkinter.tix import TCL_ALL_EVENTS
+import turtle as t
+for i in range(4):
+    t.forward(90)
+    t.left(90)
+t.penup()
+t.goto(0,30)
+t.pendown()
+ 
+t.forward(90)
+ 
+t.penup()
+t.goto(0,60)
+t.pendown()
+ 
+t.forward(90)
+ 
+t.penup()
+t.goto(30,0)
+t.pendown()
+ 
+t.left(90)
+t.forward(90)
+ 
+t.penup()
+t.goto(60,0)
+t.pendown()
+ 
+ 
+t.forward(90)
